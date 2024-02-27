8389352f1f9b4f48106eda37e3510edf3b0373c6
Laczse <lass738a@ucholstebro.dk>
2024-02-27 14:35:31 +0100

    Actually committed to Github

---
+++ publicResources/js/marketplace.js
@@ -0,0 +1,60 @@
+createItemLine();
+let price = 0;
+
+
+
+function createItemLine(){
+    var itemTypes = ["Choose Type","Armor","Gemstone","Weapons"];
+    const breakLine = document.createElement("br");
+    document.body.appendChild(breakLine);
+
+    //Create and append select list
+    var selectList = document.createElement("select");
+    selectList.setAttribute("id", "itemTypeSelect");
+    selectList.setAttribute("class", "mySelect");
+    selectList.setAttribute("data-placeholder","Choose type");
+    document.body.appendChild(selectList);
+
+    //Create and append the options
+    for (var i = 0; i < itemTypes.length; i++) {
+        var option = document.createElement("option");
+        option.setAttribute("value", itemTypes[i]);
+        option.text = itemTypes[i];
+        selectList.appendChild(option);
+    }
+    selectList.addEventListener("change",reactChosenItem);
+}
+
+function reactChosenItem(event){
+    var select = event.target;
+    console.log(select.value);
+
+    if(select.value === "Armor"){
+        var itemTypes = ["Helmet","Chestplate","Boots"];
+    }else if(select.value === "Gemstone"){
+        var itemTypes = ["Ruby","Sapphire","Emerald"];
+    } else if(select.value === "Weapons"){
+        var itemTypes = ["Crossbow","Longsword","Short Sword"];
+    }else{
+        var itemTypes = [];
+    }
+
+    //Create and append select list
+    var selectList = document.createElement("select");
+    selectList.setAttribute("id", "itemSelect");
+    selectList.setAttribute("class", "mySelect");
+    document.body.appendChild(selectList);
+
+    //Create and append the options
+    for (var i = 0; i < itemTypes.length; i++) {
+        var option = document.createElement("option");
+        option.setAttribute("value", itemTypes[i]);
+        option.text = itemTypes[i];
+        selectList.appendChild(option);
+    }
+
+
+    createItemLine();
+}
+
+
---
+++ server.js
@@ -0,0 +1,70 @@
+import process from "process";
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import e from 'express';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const rootFileSystem = process.cwd();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(express.static('publicResources'));
+//app.use('/itemImages', express.static('itemImages'));
+
+app.get('/', (req, res) => {
+  res.sendFile(rootFileSystem + "/publicResources/html/main.html");
+});
+app.get('/main.html', (req, res) => {
+  res.sendFile(rootFileSystem + "/publicResources/html/main.html");
+});
+
+app.get('/marketplace.js', (req, res) => {
+  res.sendFile(rootFileSystem + "/publicResources/js/marketplace.js");
+});
+
+app.get('/style.css', (req, res) => {
+  res.sendFile(rootFileSystem + "/publicResources/css/style.css");
+});
+
+app.get('/chosen/chosen.css', (req, res) => {
+  res.sendFile(rootFileSystem + "/chosen/chosen.css");
+});
+
+app.get('/chosen/chosen-sprite.png', (req, res) => {
+  res.sendFile(rootFileSystem + "/chosen/chosen-sprite.png");
+});
+
+app.get('/chosen/docsupport/style.css', (req, res) => {
+  res.sendFile(rootFileSystem + "/chosen/docsupport/style.css");
+});
+
+app.get('/chosen/docsupport/jquery-3.2.1.min.js', (req, res) => {
+  res.sendFile(rootFileSystem + "/chosen/docsupport/jquery-3.2.1.min.js");
+});
+
+app.get('/chosen/chosen.jquery.js', (req, res) => {
+  res.sendFile(rootFileSystem + "/chosen/chosen.jquery.js");
+});
+
+app.get('/chosen/docsupport/prism.js', (req, res) => {
+  res.sendFile(rootFileSystem + "/chosen/docsupport/prism.js");
+});
+
+app.get('/chosen/docsupport/prism.css', (req, res) => {
+  res.sendFile(rootFileSystem + "/chosen/docsupport/prism.css");
+});
+
+app.get('/chosen/docsupport/init.js', (req, res) => {
+  res.sendFile(rootFileSystem + "/chosen/docsupport/init.js");
+});
+
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
