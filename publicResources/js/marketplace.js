c334e146241ad009eff8609f01da86d366ede619
Lazcse <lassefonager@hotmail.com>
2024-02-29 11:44:19 +0100

    Small changes

--- publicResources/js/marketplace.js
+++ publicResources/js/marketplace.js
@@ -15,11 +15,6 @@ function createItemLine(){
     selectList.setAttribute("data-placeholder","Choose type");
     document.body.appendChild(selectList);
 
-    var option = document.createElement("option");
-        option.setAttribute("value", "");
-        option.text = "-";
-        selectList.appendChild(option);
-
     //Create and append the options
     for (var i = 0; i < itemTypes.length; i++) {
         var option = document.createElement("option");
@@ -47,7 +42,7 @@ function reactChosenItem(event){
     //Create and append select list
     var selectList = document.createElement("select");
     selectList.setAttribute("id", "itemSelect");
-    selectList.setAttribute("class", "chosen-select mySelect");
+    selectList.setAttribute("class", "mySelect");
     selectList.setAttribute("tabindex","-1");
 
     document.body.appendChild(selectList);
